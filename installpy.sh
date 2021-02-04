55fb2900be11033ff5b038b39d4911c25df03832
root <root@ip-172-31-34-53.us-east-2.compute.internal>
2021-02-04 06:03:26 +0000

    Python

---
+++ installpy.sh
@@ -0,0 +1,7 @@
+procedure to download python 
+
+yum list|grep python
+in that list select "python3-devel.x86_64"
+use command
+yum install "python3-devel.x84_64" -y
+Then python3 is installed in the VM
